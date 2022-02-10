efb6f206b245e0c520507523519e4e04f6b55ead
Meet Madhu <meet.madhu@MBProC-MeetM.hitronhub.home>
2022-02-09 21:36:50 -0800

    basic header and main add

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import mealImage from "../../assets/meals.jpeg";
+import classes from "./Header.module.css";
+
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealImage} alt="A table full of delicious food!" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
